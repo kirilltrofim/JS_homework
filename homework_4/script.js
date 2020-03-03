5a2eedbca487928751ce6ba03b6a3004c9a26af4
Kirill <kirill.trofim@gmail.com>
2020-03-03 22:56:04 +0200

    1

--- homework_4/script.js
+++ homework_4/script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 let money, time;
 
 function start() {
@@ -18,67 +19,64 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    saving: true
-};
+    saving: true,
 
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Введите обязательну статью расходов в этом месяце", ''),
-            b = prompt("Во сколько обойдеться?", '');
-        if (typeof (a) == 'string' && (typeof (a) != null) && (typeof (b) != null) && a != '' && b != '' && a.length < 50) {
-            console.log("done");
-            appData.expenses[a] = b;
-        } else {
-            console.log("bad result");
-            i--;
+
+    chooseExpenses: function () {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("Введите обязательну статью расходов в этом месяце", ''),
+                b = prompt("Во сколько обойдеться?", '');
+            if (typeof (a) == 'string' && (typeof (a) != null) && (typeof (b) != null) && a != '' && b != '' && a.length < 50) {
+                console.log("done");
+                appData.expenses[a] = b;
+            } else {
+                console.log("bad result");
+                i--;
+            }
         }
+    },
+
+
+    checkSavings: function () {
+        if (appData.saving == true) {
+            let save = +prompt("Какова сумма накоплений?"),
+                percent = +prompt("Под какой процент?");
+
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+        }
+    },
+
+
+    detectDayBudget: function () {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        alert("Бюджет на 1 день: " + appData.moneyPerDay + "руб.");
+    },
+
+
+    detectLevel: function () {
+        if (appData.moneyPerDay < 100) {
+            console.log("MIN");
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log("MIDDLE");
+        } else if (appData.moneyPerDay > 2000) {
+            console.log("MAX");
+        } else console.log("ERROR");
+    },
+
+
+    chooseOptExpenses: function () {
+        for (let i = 0; i < 3; i++) {
+            let questionOptExpenses = prompt("Статья необязательных расходов?");
+            appData.optionalExpenses[i] = questionOptExpenses;
+            console.log(appData.optionalExpenses);
+        }
+    },
+
+    chooseIncome: function(){
+        let items = prompt("Что принесет дополнительный доход? (Перечислете через запятую)","");
+        appData.income = items.split(', ');
+        appData.income.push(prompt('Может что-то еще?'));
+        appData.income.sort();
     }
-}
-chooseExpenses();
-
-
-
-
-function checkSavings() {
-    if (appData.saving == true) {
-        let save = +prompt("Какова сумма накоплений?"),
-            percent = +prompt("Под какой процент?");
-
-        appData.monthIncome = save / 100 / 12 * percent;
-        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
-    }
-}
-checkSavings();
-
-function detectDayBudget() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
-
-
-    alert("Бюджет на 1 день: " + appData.moneyPerDay + "руб.");
-}
-detectDayBudget();
-
-function detectLevel() {
-    if (appData.moneyPerDay < 100) {
-        console.log("MIN");
-    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-        console.log("MIDDLE");
-    } else if (appData.moneyPerDay > 2000) {
-        console.log("MAX");
-    } else console.log("ERROR");
-}
-detectLevel();
-
-function chooseOptExpenses() {
-    for (let i = 0; i < 3; i++) {
-        let questionOptExpenses = prompt("Статья необязательных расходов?");
-        appData.optionalExpenses[i] = questionOptExpenses;
-        console.log(appData.optionalExpenses);
-    }
-}
-
-function printArr() {
-    appData.forEach( function (item){
-        console.log(item);
-    }) 
-}
+};      // сделать дз
